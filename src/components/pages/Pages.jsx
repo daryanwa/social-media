2c683a33cae03d4a1234c3c7eb49d122ebb7f917
Rostislav Parahnenko <asawww100@gmail.com>
2024-01-13 19:40:13 +0200

    routes

--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import Home from './Home'
-import {Routes, Route, BrowserRouter } from 'react-router-dom'
+import {Routes, Route } from 'react-router-dom'
 import Login from './Login'
 
 
 function Pages() {
   return (
     <div>
-      <BrowserRouter >
+    
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/login' element={<Login />} />
         </Routes>
-      </BrowserRouter >
+      
     </div>
   )
 }
